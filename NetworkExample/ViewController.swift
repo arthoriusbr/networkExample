bcfa8c3dfdc44350e3ed0dc10533efc8400f9d14
administrador <administrador@Mac-mini-de-administrador.local>
2018-07-16 11:50:26 -0300

    busca de repositório no github através da URL - realizada a primeira requisição http (get)

--- NetworkExample/ViewController.swift
+++ NetworkExample/ViewController.swift
@@ -7,19 +7,34 @@
 //
 
 import UIKit
+import Alamofire
+import AlamofireObjectMapper
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func alamofireExample(_ sender: Any) {
+        
+        Alamofire.request("https://api.github.com/users/arthoriusbr/repos", method: .get).validate().responseArray(completionHandler: { (response: DataResponse<[Repo]>) in
+            switch response.result {
+            case .success:
+                if let repos = response.result.value{
+                    for repo in repos {
+                        print("Nome: \(repo.name ?? "") e foi feito em \(repo.language ?? "")")
+                    }
+                }
+
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        })
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func soma (x val1: Int, y val2: Int = 2) -> Int {
+        return val1 + val2
     }
-
-
 }
 
